26a03bf11ef23518fbdd4644188cd900dc2574d4
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-17 08:29:35 +0800

    AM 01st commit

---
+++ springboot_mybatis-plus_01/src/main/java/com/itheima/SpringbootMybatisPlus01Application.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisPlus01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisPlus01Application.class, args);
+    }
+
+}
---
+++ springboot_mybatis-plus_01/src/main/java/com/itheima/pojo/AnimeInfo.java
@@ -0,0 +1,26 @@
+package com.itheima.pojo;
+
+import lombok.Data;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> springboot_first_01
+ * BelongsPackage ==> com.itheima.pojo
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-17 08:27:31
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Data
+public class AnimeInfo {
+
+    private String name;
+
+    private Integer releaseTime;
+
+    private String character01;
+
+    private String character02;
+
+    private Integer id;
+
+}
