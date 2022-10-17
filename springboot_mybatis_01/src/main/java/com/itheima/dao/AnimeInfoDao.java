6f114c2ae1e95a7a039f716071490cc5d4c025f3
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-17 08:18:06 +0800

    AM 01st commit

---
+++ springboot_mybatis_01/src/main/java/com/itheima/dao/AnimeInfoDao.java
@@ -0,0 +1,23 @@
+package com.itheima.dao;
+
+import com.itheima.pojo.AnimeInfo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> springboot_first_01
+ * BelongsPackage ==> com.itheima.dao
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-17 08:15:44
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Mapper
+public interface AnimeInfoDao {
+
+    @Select("select * from anime_info")
+    List<AnimeInfo> selectAll();
+
+}
