88992b229191aaf2ed4864a388dcd5ffeed97eb2
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-18 08:38:34 +0800

    AM 01st commit

--- springboot_mybatis-plus_01/src/main/java/com/itheima/SpringbootMybatisPlus01Application.java
+++ springboot_mybatis-plus_01/src/main/java/com/itheima/SpringbootMybatisPlus01Application.java
@@ -1,9 +1,10 @@
 package com.itheima;
 
+import com.itheima.config.MpConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MpConfig.class})
 public class SpringbootMybatisPlus01Application {
 
     public static void main(String[] args) {
